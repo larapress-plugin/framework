10030d86d793aae3516620b36b74cce4cf1247e1
Moahmed Zaki <mohamed.zaki@eye-ltd.com>
2018-12-17 00:56:08 +0200

    change namespace and main path

---
+++ src/Validation/ValidationServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LarapressPlugin\Validation;
+
+use Illuminate\Validation\ValidationServiceProvider as ServiceProvider;
+
+class ValidationServiceProvider extends ServiceProvider{
+    protected function registerPresenceVerifier(){}
+}
